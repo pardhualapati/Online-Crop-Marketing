e3cbbe0bb867a19aea0cb1435d85ed9991fab7ef
MrPavalion <pardhualapati01@gmail.com>
2022-12-14 17:47:31 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const passport = require("passport");
+const session = require("express-session");
+const passportLocalSession = require("passport-local-mongoose");
 const app = express();
+
 app.use(express.static("public"));
+//ejs
 app.set('view engine', 'ejs');
+//body-parser
 app.use(bodyParser.urlencoded({
   extended: true
 }))
@@ -19,12 +26,29 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-//Using the mongoose encryption
-console.log(process.env.SECRET);
-userSchema.plugin(encrypt, {secret:process.env.SECRET , encryptedFields:['password']}) ;
-
 
 const User = new mongoose.model("User", userSchema);
+//Using the mongoose encryption
+//mongo schema for field data
+
+const postSchema = mongoose.Schema({
+  fullName: { type: String, required: false, set: a => a === '' ? undefined : a},
+  fieldEmail: { type: String, required: false, set: b => b === '' ? undefined : b},
+  phNo: { type: String, required: false, set: c => c === '' ? undefined : c},
+  state: { type: String, required: false, set: d => d === '' ? undefined : d},
+  zip : { type: String, required: false, set: e => e === '' ? undefined : e},
+  address: { type: String, required: false, set: e => e === '' ? undefined : e},
+  cropName: { type: String, required: false, set: f => f === '' ? undefined : f},
+  cropregion: { type: String, required: false, set: g => g === '' ? undefined : g},
+  cropQuantity:{ type: String, required: false, set: h => h === '' ? undefined : h},
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
+
+
 
 app.get("/", function(req, res) {
 
@@ -39,57 +63,132 @@ app.get("/login", function(req, res) {
 app.get("/register", function(req, res) {
   res.render("register");
 });
-app.get("/secrets", function(req, res) {
+app.get("/main", function(req, res) {
 
-  res.render("secrets");
+  res.render("main");
 })
+app.get("/aboutus", function(req, res) {
 
-app.get("/logout",function(req,res){
+  res.render("aboutus");
+});
+app.get("/logout", function(req, res) {
 
   res.render("home");
 });
+app.get("/submit", function(req, res) {
+
+  res.render("compose");
+})
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+app.get("/contactus",function(req,res){
+  res.render("contactus");
+});
+
+
+app.get("/search" , function(req,res){
+
+    res.render("search");
+});
+
+
 
 app.post("/register", function(req, res) {
 
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("main");
+      }
+    });
   });
-  newUser.save(function(err) {
-    if (err) {
-      console.log( err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
 });
 
-app.post("/login",function(req,res){
+app.post("/login", function(req, res) {
 
-      const username = req.body.username
-      const password = req.body.password
+  const username = req.body.username;
+  const password = req.body.password;
 
-      User.findOne({ email:username},function(err , foundUser){
-        if(err){
-          console.log(err);
-        }
-        else{
-          if(foundUser){
-            if(foundUser.password === password){
-              res.render("secrets");
-            }
-            else{
-              console.log("Password Not Correct");
-            }
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("main");
           }
+        });
+
+      } else {
+        console.log("Password Not Correct");
+      }
+    }
 
 
-        }
-      })
 
-})
+  });
+
+});
 
 
+
+
+app.post("/compose",function(req,res){
+
+    const post = new Post({
+      fullName: req.body.fullName,
+      fieldEmail: req.body.fieldEmail,
+      phNo: req.body.phnno,
+      state: req.body.state,
+      zip :req.body.pincode,
+      address:req.body.address,
+      cropName:req.body.cropName,
+      cropregion:req.body.cropregion,
+      cropQuantity:req.body.cropQuantity
+
+    });
+    post.save();
+    res.redirect('/main');
+});
+
+
+app.post("/search",function(req,res){
+
+  const cropname = req.body.cropName;
+  const regionarea = req.body.region;
+
+  Post.find({
+    cropName: cropname,
+    cropregion : regionarea
+  }, function(err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+
+      if(Object.keys(foundPost).length>0){
+
+        res.render("sucess" ,{users:foundPost});
+      }
+      else{
+        res.render("failure");
+      }
+
+      }
+    });
+
+});
 
 app.listen(3000, function() {
   console.log("Server started on Port 3000");
